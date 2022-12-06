3ace98184cb5495e6fcc98e70a6f3489c494786b
Jacob Downarowicz <jakubowarski@gmail.com>
2022-12-05 23:11:53 -0500

    Header Update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 /*********************************************************************************
- * WEB422 – Assignment 5
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
+ * WEB422 – Assignment 06
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+ * assignment has been copied manually or electronically from any other source (including web sites) or
+ * distributed to other students.
  *
- * Name: Jacob Downarowicz Student ID: 135396208 Date: 11/18/2022
+ * Name: Jacob Downarowicz Student ID: 135396208 Date: 12/5/2022
  *
+ * Vercel App (Deployed) Link: https://a6-jacob-g5jc4kzwa-jdownarowicz-mysenecaca.vercel.app/
  *
  ********************************************************************************/
 
